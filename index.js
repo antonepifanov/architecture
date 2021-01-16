8c095b17e1a6d28729ea6bc42cb7b126e99fffd9
antonepifanov <antonepifanov87@gmail.com>
2021-01-16 14:27:38 +0300

    Адаптивная навигация+заголовок

---
+++ index.js
@@ -0,0 +1,13 @@
+(function($) { 
+  $(function() { 
+    $('.header__nav-toggle').click(function() {
+      $('.nav-list').slideToggle({
+        start: function(){
+          jQuery(this).css('display','flex');}
+        })
+      });
+      $('.header__nav-toggle').on('click', function() {
+       this.classList.toggle('active');
+    });
+  }); 
+})(jQuery);
